77ad3d96ac75d0ab767eaf68c91ea3d91af48d61
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-19 00:10:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,15 +2,18 @@ require 'pry'
 
 
 def run_guessing_game
+  
 comp_num = rand(6) + 1
+
 input = gets.chomp.to_s
+
 leave = "exit"
+
 if input == comp_num
   puts "You guessed the correct number!"
 elsif input != comp_num && input != leave.to_s
-puts "Sorry! The computer guessed #{comp_num}"
+  puts "Sorry! The computer guessed #{comp_num}"
+elsif input == leave
+  puts "Goodbye!"
 end
-  
-  
-  
 end
