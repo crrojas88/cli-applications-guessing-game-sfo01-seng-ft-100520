51c6fffe355f19627cf257e044568eeb21bb0fb6
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-19 00:11:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,6 +10,7 @@ input = gets.chomp.to_s
 leave = "exit"
 
 if input == comp_num
+  binding.pry
   puts "You guessed the correct number!"
 elsif input != comp_num && input != leave.to_s
   puts "Sorry! The computer guessed #{comp_num}"
