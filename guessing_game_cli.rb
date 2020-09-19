d15f4977338ff802f79b8e1221e94e43664fdb04
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-19 00:44:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,14 +5,14 @@ def run_guessing_game
 
 comp_num =  (1 + rand(6)).to_s
 
-input = gets.chomp.to_s
+user_input = gets.chomp.to_s
 
 leave = "exit"
 
-if input == comp_num
+if user_input == comp_num
   puts "You guessed the correct number!"
   
-elsif input != comp_num && input != leave.to_s
+elsif user_input != comp_num && user_input != leave.to_s
   puts "Sorry! The computer guessed #{comp_num}."
   
 elsif input == leave
