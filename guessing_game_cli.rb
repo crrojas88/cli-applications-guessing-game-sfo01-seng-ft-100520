99a23345fc51b724749659d88aaa613d2c06c060
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-19 00:48:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -15,7 +15,7 @@ if user_input == comp_num
 elsif user_input != comp_num && user_input != leave.to_s
   puts "Sorry! The computer guessed #{comp_num}."
   
-elsif input == leave
+elsif user_input == leave
   puts "Goodbye!"
   
   end
